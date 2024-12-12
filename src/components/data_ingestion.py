c2818db113dffeb676f246edc3f5819a0aee216a
saifulislamsarfaraz <saifulislamsarfaraz@gmail.com>
2024-12-12 18:00:25 +0600

    data ingestions

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -34,5 +34,15 @@ class DataIngestion:
             train_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
 
             logging.info("Ingestion of data is completed")
-        except:
-            pass
+
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+        
+        except Exception as e:
+            raise CustomException(e, sys)
+        
+if __name__=="__main__":
+    obj = DataIngestion()
+    obj.initiate_data_ingestion()
