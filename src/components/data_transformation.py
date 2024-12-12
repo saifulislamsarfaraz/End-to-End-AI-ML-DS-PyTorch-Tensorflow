b05e9a7659405d17b6eadae1372cf392624f834b
saifulislamsarfaraz <saifulislamsarfaraz@gmail.com>
2024-12-12 18:24:29 +0600

    data Transformation

---
+++ src/components/data_transformation.py
@@ -0,0 +1,35 @@
+import sys
+from dataclasses import dataclass
+import numpy as np
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from src.exception import CustomException
+from src.logger import logging
+import os
+
+
+@dataclass
+class DataTransformationConfig:
+    preprocessor_obj_file_path = os.path.join('artifacts', 'preprocessor.pkl')
+
+class DataTransformation:
+    def __init__(self):
+        self.data_transformation_config = DataTransformationConfig()
+
+    def get_data_transformer_object(self):
+        try:
+            numerical_columns = ["writing score", "reading score"]
+            categorical_columns = [
+                "gender",
+                "race_ethnicity",
+                "parental_level_of_education",
+                "lunch",
+                "test_preparation_course",
+            ]
+
+            pass
+        except Exception as e:
+            raise CustomException(e, sys)
